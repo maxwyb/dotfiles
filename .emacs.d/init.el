aa1d3472a66ddf191c58cea23dc1cd518594a9e9
Yingbo (Max) Wang <maxwyb@gmail.com>
2017-07-24 15:09:39 -0700

    Add multi-web-mode in Emacs.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -196,3 +196,14 @@
 (use-package go-mode
   :ensure t)
 
+;; multi-web-mode for front-end web development
+(use-package multi-web-mode
+  :ensure t
+  :config
+  (setq mweb-default-major-mode 'html-mode)
+  (setq mweb-tags '((php-mode "<\\?php\\|<\\? \\|<\\?=" "\\?>")
+		    (js-mode "<script +\\(type=\"text/javascript\"\\|language=\"javascript\"\\)[^>]*>" "</script>")
+		    (css-mode "<style +type=\"text/css\"[^>]*>" "</style>")))
+  (setq mweb-filename-extensions '("php" "htm" "html" "ctp" "phtml" "php4" "php5"))
+  (multi-web-global-mode 1))
+
