8ea604032d447594367a00753af2bf2748885900
Yingbo (Max) Wang <maxwyb@gmail.com>
2018-01-18 14:45:54 -0800

    Add Java IDE in Emacs using emacs-eclim.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -253,6 +253,22 @@
     :config
     (add-to-list 'company-backends 'company-tern)))
 
+;; Java IDE by emacs-eclim with eclim backend
+(setq eclim-eclipse-dirs "~/eclipse/java-oxygen/Eclipse.app/Contents/MacOS/eclipse")
+(setq eclim-executable "~/.p2/pool/plugins/org.eclim_2.7.1/bin/eclim")
+(setq eclimd-autostart t)
+(defun my-java-mode-hook ()
+    (eclim-mode t))
+(add-hook 'java-mode-hook 'my-java-mode-hook)
+(use-package eclim
+  :ensure t
+  :commands eclim-mode
+  :config
+  (use-package company-emacs-eclim
+    :ensure t
+    :config
+    (company-emacs-eclim-setup)))
+  
 ;; [Debugging backgrace]
 ;; (setq max-specpdl-size 5)  ; default is 1000, reduce the backtrace level
 ;; (setq debug-on-error t)    ; now you should get a backtrace
